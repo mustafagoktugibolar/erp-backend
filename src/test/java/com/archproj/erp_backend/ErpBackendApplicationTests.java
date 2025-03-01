8c682e39bb29d287ed327490cba97156d105ed56
Mustafa Göktuğ İbolar <goktugibolar@icloud.com>
2025-03-01 14:55:12 +0300

    Project Created

---
+++ src/test/java/com/archproj/erp_backend/ErpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archproj.erp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
